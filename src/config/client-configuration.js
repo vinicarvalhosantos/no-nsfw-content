83f0642f6b618930a2442b67cdde6d7a6371565b
Vinicius Santos <vinicius.santos@ernetwork.com.br>
2021-03-23 20:11:48 -0300

    Class module export refactored

--- src/config/client-configuration.js
+++ src/config/client-configuration.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 
-function clientConfiguration(client) {
-
+module.exports = (client) => {
     client.login(TOKEN);
 
     client.on('ready', () => {
         console.info(`Logged in as ${client.user.tag}!`);
     });
-
-
 }
-
-module.exports = exports = clientConfiguration;
