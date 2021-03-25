1f86a6dfa2aaa5b701b2a97b314031d72d4b904d
Vinicius Santos <vinicius.santos@ernetwork.com.br>
2021-03-25 14:54:29 -0300

    Add some classes documentation

--- src/utils/check-image.utils.js
+++ src/utils/check-image.utils.js
@@ -2,6 +2,20 @@ const Discord = require('discord.js');
 const checkImageContent = require('../http-service/check-image.http-service');
 const textMessage = require('../text/index');
 
+
+/**
+ * This method is responsable to make some prepares and send the image url to check.
+ * Method Steps:
+ *  - Delete the original message;
+ *  - Reply the message owner that we analysing the image sent
+ *  - Send url to the API to check image content
+ *  - Check if API return error
+ *    - If not: Send the embed message
+ *    - If returns error: Delete the reply message
+ * 
+ * @param {Discord.Message} message The discord message object
+ * @param {String} image The image url
+ */
 const prepareImageContentToCheck = async (message, image) => {
     message.delete();
     const messageWaiting = await message.reply(textMessage.getAnalysingMessage());
@@ -13,6 +27,14 @@ const prepareImageContentToCheck = async (message, image) => {
     }
 }
 
+/**
+ * This method is responsable to check if an image was sended
+ * Method Steps:
+ *  - Try to extract an image url by message attachments
+ *  - Check if it was extracted, send the url to prepare
+ * 
+ * @param {Discord.Message} message Discord message object
+ */
 const checkSendedImage = async (message) => {
     const image = await message.attachments.size > 0 ? message.attachments.array()[0].proxyURL : null;
     if (image) {
@@ -20,6 +42,24 @@ const checkSendedImage = async (message) => {
     }
 }
 
+/**
+ * This method is responsable to send final embed message
+ * Method Steps:
+ *  - Delete the reply message
+ *  - Check if the image sent is a nsfw content
+ *  - If it is:
+ *    - Create a embed message with color and the image sent
+ *    - Send this embed message to the chat
+ *  - If it's not:
+ *    - Create a embed message with color, the message that the image sent is a nsfw content, add fields with action that is possible to happen(Timeout, kick and ban)
+ *    - Send this embed message to the chat
+ *    - React to this message with number of actions
+ * 
+ * @param {String} urlImage Image url
+ * @param {Discord.Message} message Discord message object
+ * @param {Discord.Message} messageWaiting Discord message object
+ * @param {Boolean} isNsfw If the image is a nsfw content
+ */
 const sendEmbedMessage = async (urlImage, message, messageWaiting, isNsfw = false) => {
     messageWaiting.delete();
     if (!isNsfw) {
@@ -46,6 +86,13 @@ const sendEmbedMessage = async (urlImage, message, messageWaiting, isNsfw = fals
     }
 }
 
+/**
+ * This method is responsable to check if a url is an image or not
+ * 
+ * @param {String} urlImage Image url
+ * @returns {Boolean} Returns true if its an image and false if its not
+ * 
+ */
 const urlIsJpgPngOrGif = (urlImage) => {
     const urlSplitted = urlImage.split('/');
     const imageName = urlSplitted[urlSplitted.length - 1];
@@ -57,11 +104,23 @@ const urlIsJpgPngOrGif = (urlImage) => {
     return false;
 }
 
+/**
+ * This method is responsable to check if a string is a url
+ * 
+ * @param {String} message String sent by user 
+ * @returns {Boolean} Returns true if it is a url and false if its not
+ */
 const haveUrl = (message) => {
     const pattern = new RegExp('([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?');
     return pattern.test(message);
 }
 
+/**
+ * This method is responsable to check if it is a gif
+ * 
+ * @param {Discord.Embed} embed String sent by user 
+ * @returns {Boolean} Returns true if it is a gif and false if its not
+ */
 const isGif = (embed) => {
     if (embed && embed.type) return embed.type === 'gifv';
     return false;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,26 @@
+/**
+ * This method is responsable to return wich of params is higher.
+ * 
+ * @param {Number} firstValue First number to compare.
+ * @param {Number} secondValue Second number to compare.
+ * @returns {Number} Returns the highest number between params.
+ * @example
+ *      getHighestValue(10, 20);
+ */
+
 const getHighestValue = (firstValue, secondValue) => {
     return firstValue > secondValue ? firstValue : secondValue;
 }
 
+/**
+ * This method is responsable to check if a discord member has a role.
+ * 
+ * @param {Discord.Member} member A discord member object wich contains the role name and id and some other things not important to this function.
+ * @param {Array} role A list with with roles to check if member has it.
+ * @returns {Boolean} Returns true if member has the role passed by function and false if not.
+ * @example
+ *     checkIfMemberHasRole(member, ['Moderator']); 
+ */
 const checkIfMemberHasRole = (member, role) => {
     if (member.roles.cache.some(myRole => role.includes(myRole.name))) return true;
     return false;
