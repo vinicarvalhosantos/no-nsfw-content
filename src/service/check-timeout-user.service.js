3496778086e68028907d45f9eca0cb1ac8c39b2a
Vinicius Santos <vinicius.santos@ernetwork.com.br>
2021-03-29 15:14:44 -0300

    When user in timeout join a voice channel, kick him out

--- src/service/check-timeout-user.service.js
+++ src/service/check-timeout-user.service.js
@@ -1,10 +1,18 @@
 const utils = require('../utils/index');
+const TIMEOUTROLES = ['In Timeout']
 
 
 module.exports = (client) => {
     client.on('message', async message => {
-        if (utils.checkIfMemberHasRole(message.member, ['In Timeout'])) {
+        if (utils.checkIfMemberHasRole(message.member, TIMEOUTROLES)) {
             message.delete();
         }
     });
+
+    client.on('voiceStateUpdate', async (oldMember, newMember) => {
+        const member = newMember.guild.members.cache.find(member => member.id === newMember.id);
+        if (utils.checkIfMemberHasRole(member, TIMEOUTROLES)) {
+            newMember.member.voice.kick();
+        }
+    })
 }
